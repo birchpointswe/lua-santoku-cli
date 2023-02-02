3c52aef39eed0e02e42276b944c0a6dce8a9f319
broma0 <121064585+broma0@users.noreply.github.com>
2023-02-01 20:56:21 -0500

    co.lua tuple.lua: update coroutine.create, coroutine.resume +9
    src/santoku/co.lua
      - make
      ~ coroutine.create, coroutine.resume, coroutine.wrap, coroutine.yield, for_resume, for_wrap
    src/santoku/tuple.lua
      + get, helper, set, tuple
    
    also: santoku-0.0.9-1.rockspec, src/santoku/err.lua, src/santoku/gen.lua, test/spec/santoku/tuple.lua

---
+++ src/santoku/tuple.lua
@@ -0,0 +1,33 @@
+local co = require("santoku.co")
+
+local function tuple ()
+  local co = co()
+  local function helper (...)
+    co.yield(...)
+    return helper(co.yield(...))
+  end
+  local cor = coroutine.create(helper)
+  return function (...)
+    return select(2, co.resume(cor, ...))
+  end
+end
+
+
+return function (...)
+  local active = tuple()
+  local inactive = tuple()
+  active(...)
+  return {
+
+    set = function (...)
+      inactive(active())
+      active(...)
+      return inactive()
+    end,
+
+    get = function (i)
+      active, inactive = inactive, active
+      return select(i or 1, active(inactive()))
+    end
+  }
+end
