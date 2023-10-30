cc2e9babebb4ac54e1518b3c7fd9c6912dd1a914
broma0 <121064585+broma0@users.noreply.github.com>
2023-10-30 11:15:47 -0400

    santoku-cli.lua template.lua test.c toku.lua: add get_config +6
    bin/toku.lua
      + get_config, process_file, process_files, write_deps
    src/santoku-cli.lua
      - get_config, process_file, process_files, write_deps
    src/santoku/template.lua
      + M._should_include
    test/res/bundle/test/test.c
      + main, reader
    
    also: .gitignore, Makefile, config.lua, config/cli.mk, config/cli.rockspec, config/lib.mk, config/lib.rockspec, docs/todo.md, src/santoku/gen.lua, test/luacheck.lua, test/luacov.lua, test/res/bundle/test.lua, test/res/bundle/test/test, test/res/bundle/test/test.h, test/res/bundle/test/test.lua, test/res/bundle/test/test.luac, test/res/fs.tst1.txt, test/res/fs.tst2.txt, test/res/fs/a/a.txt, test/res/fs/a/b.txt, test/res/fs/b/a.txt, test/res/fs/b/b.txt, test/res/geo/map.pdf, test/res/template/body-content.html, test/res/template/body.html, test/res/template/config.lua, test/res/template/head.html, test/res/template/index.html, test/res/template/name.html, test/res/template/title.html, test/res/template/titles.html, test/spec/santoku/async.lua, test/spec/santoku/bundle.lua, test/spec/santoku/bundle/test.lua, test/spec/santoku/cli/template.lua, test/spec/santoku/compat.lua, test/spec/santoku/css.lua, test/spec/santoku/env.lua, test/spec/santoku/fs.lua, test/spec/santoku/fs.tst1.txt, test/spec/santoku/fs.tst2.txt, test/spec/santoku/fs/a/a.txt, test/spec/santoku/fs/a/b.txt, test/spec/santoku/fs/b/a.txt, test/spec/santoku/fs/b/b.txt, test/spec/santoku/fun.lua, test/spec/santoku/gen.lua, test/spec/santoku/geo.lua, test/spec/santoku/geo/pdf.lua, test/spec/santoku/html.lua, test/spec/santoku/inherit.lua, test/spec/santoku/map.pdf, test/spec/santoku/table.lua, test/spec/santoku/template.lua, test/spec/santoku/template/body-content.html, test/spec/santoku/template/body.html, test/spec/santoku/template/config.lua, test/spec/santoku/template/head.html, test/spec/santoku/template/index.html, test/spec/santoku/template/name.html, test/spec/santoku/template/title.html, test/spec/santoku/template/titles.html

---
+++ config.lua
@@ -0,0 +1,54 @@
+local _ENV = {}
+
+name = "santoku"
+version = "0.0.94-1"
+variable_prefix = "TK"
+
+license = "MIT"
+
+luacov_include = {
+  "^%./santoku.*"
+}
+
+
+
+
+
+
+
+
+dependencies = {
+
+  "lua >= 5.1",
+
+
+
+
+
+
+
+
+}
+
+test_dependencies = {
+  "luacov >= 0.15.0",
+  "luacheck >= 1.1.0-1",
+  "lua-zlib >= 1.2-2",
+  "luafilesystem >= 1.8.0-1",
+  "lsqlite3 >= 0.9.5",
+  "inspect >= 3.1.3-0",
+  "luassert >= 1.9.0-1"
+}
+
+homepage = "https://github.com/treadwelllane/lua-" .. name
+tarball = name .. "-" .. version .. ".tar.gz"
+download = homepage .. "/releases/download/" .. version .. "/" .. tarball
+
+return {
+  env = _ENV,
+  excludes = {
+    "src/santoku/template.lua",
+    "test/spec/santoku/template.lua",
+    "test/spec/santoku/cli/template.lua"
+  },
+}
