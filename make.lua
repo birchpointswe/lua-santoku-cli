c7a2206b346ceab2180eaff66d31115744ef3218
broma0 <121064585+broma0@users.noreply.github.com>
2026-08-03 14:30:25 -0400

    bump version to 1.0.1-1

--- make.lua
+++ make.lua
@@ -1,6 +1,6 @@
 local env = {
   name = "santoku-cli",
-  version = "1.0.0-1",
+  version = "1.0.1-1",
   variable_prefix = "TK_CLI",
   license = "MIT",
   public = true,
@@ -12,7 +12,7 @@ local env = {
     "santoku-bundle >= 1.0.0, < 2.0.0",
     "santoku-system >= 1.0.0, < 2.0.0",
     "santoku-test-runner >= 1.0.0, < 2.0.0",
-    "santoku-make >= 1.0.0, < 2.0.0",
+    "santoku-make >= 1.0.2, < 2.0.0",
     "santoku-mustache >= 1.0.0, < 2.0.0",
     "argparse >= 0.7.1-1",
   },
